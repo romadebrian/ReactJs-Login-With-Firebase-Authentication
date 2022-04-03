ad41f82e8770a5f9de9e37d01ee27669b5ef8307
romadebrian <roma_coll04@yahoo.com>
2022-04-04 00:38:18 +0700

    resolve error Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in the componentWillUnmount method.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,17 +3,27 @@ import { auth } from "../../config/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
 export default class Home extends Component {
+  _isMounted = false;
+
   state = {
     user: {},
   };
 
   componentDidMount() {
+    this._isMounted = true;
+
     onAuthStateChanged(auth, (currentUser) => {
       // console.log(currentUser);
-      this.setState({
-        user: currentUser,
-      });
+      if (this._isMounted) {
+        this.setState({
+          user: currentUser,
+        });
+      }
     });
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
   }
 
   render() {
