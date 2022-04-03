8d2f5a413c1ffd9279c5dd9068f4e2ec68357300
romadebrian <roma_coll04@yahoo.com>
2022-04-04 00:14:54 +0700

    add onAuthStateChanged and send data current user to App.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+onAuthStateChanged(auth, (currentUser) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App dataUser={currentUser} />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
